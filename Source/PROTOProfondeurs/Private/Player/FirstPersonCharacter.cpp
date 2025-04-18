02535f8d09cc558d9e6700751bcfd8f46bede7c5
Melvin <74654895+MichenaudMelvin@users.noreply.github.com>
2025-04-18 14:47:27 +0200

    [ADD] Nerve function

--- Source/PROTOProfondeurs/Private/Player/FirstPersonCharacter.cpp
+++ Source/PROTOProfondeurs/Private/Player/FirstPersonCharacter.cpp
@@ -524,17 +524,3 @@ bool AFirstPersonCharacter::IsStopped() const
 }
 
 #pragma endregion
-
-#if WITH_EDITORONLY_DATA
-
-void AFirstPersonCharacter::ChangeState(const ECharacterStateID& State) const
-{
-	if (!StateMachine)
-	{
-		return;
-	}
-
-	StateMachine->ChangeState(State);
-}
-
-#endif
