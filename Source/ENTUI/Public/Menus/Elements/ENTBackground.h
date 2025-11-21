9077e3401150fc07baf2462144e90811ea830185
Dan <112936289+DanZo0x@users.noreply.github.com>
2025-11-21 17:56:53 +0100

    [EDIT] Background fade animations TO CLEAN

--- Source/ENTUI/Public/Menus/Elements/ENTBackground.h
+++ Source/ENTUI/Public/Menus/Elements/ENTBackground.h
@@ -5,17 +5,32 @@
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
 #include "Components/Image.h"
+#include "Menus/ENTWidgetBasics.h"
 #include "ENTBackground.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnBackgroundFadeAnimationFinished, UUserWidget*, Menu);
 
 /**
  * 
  */
 UCLASS()
-class ENTUI_API UENTBackground : public UUserWidget
+class ENTUI_API UENTBackground : public UENTWidgetBasics
 {
 	GENERATED_BODY()
 
+public:
+	// UPROPERTY(BlueprintAssignable, BlueprintCallable)
+	// FOnBackgroundFadeAnimationFinished OnBackgroundFadeAnimationFinished;
+	//
+	// UFUNCTION(BlueprintCallable)
+	// void PlayFadeAnimation(bool bIsReverse);
+
 protected:
+	// UPROPERTY(BlueprintReadOnly, Transient, meta = (BindWidgetAnimOptional))
+	// TObjectPtr<UWidgetAnimation> FadeAnimation;
+	//
+	// FWidgetAnimationStatePlayParams PlayParams;
+	
 	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
 	TObjectPtr<UImage> BackgroundImage;
 };
