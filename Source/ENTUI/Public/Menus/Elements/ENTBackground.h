69c4ed3bd83b490dc0dc282f3d3dd64c9af900bb
Dan <112936289+DanZo0x@users.noreply.github.com>
2025-11-24 00:59:03 +0100

    [EDIT] UI background class cleanup + y'a moy j'ai fix rider (pitié)

--- Source/ENTUI/Public/Menus/Elements/ENTBackground.h
+++ Source/ENTUI/Public/Menus/Elements/ENTBackground.h
@@ -18,19 +18,7 @@ class ENTUI_API UENTBackground : public UENTWidgetBasics
 {
 	GENERATED_BODY()
 
-public:
-	// UPROPERTY(BlueprintAssignable, BlueprintCallable)
-	// FOnBackgroundFadeAnimationFinished OnBackgroundFadeAnimationFinished;
-	//
-	// UFUNCTION(BlueprintCallable)
-	// void PlayFadeAnimation(bool bIsReverse);
-
 protected:
-	// UPROPERTY(BlueprintReadOnly, Transient, meta = (BindWidgetAnimOptional))
-	// TObjectPtr<UWidgetAnimation> FadeAnimation;
-	//
-	// FWidgetAnimationStatePlayParams PlayParams;
-	
 	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
 	TObjectPtr<UImage> BackgroundImage;
 };
